ebea6a30e3c82f1ca18fce5cef0d460b27276c4e
puge <mail@puge.org>
2020-03-26 11:40:07 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -1,39 +1,3 @@
-// 设计宽度
-var deviseW = 750
-// 设计高度
-var deviseH = 1508
-
-function getScale () {
-  const scaleBox = document.getElementsByClassName('scale-box')[0]
-  // 如果比例大于1则进入电脑模式
-  if ((window.innerWidth / window.innerHeight) < 1) {
-    var scale = window.innerWidth / deviseW
-    scaleBox.style.width = deviseW + 'px'
-    scaleBox.style.height = deviseH + 'px'
-    scaleBox.style.transform = `scale(${scale}, ${scale})`
-    // console.log(window.innerHeight, deviseH * scale)
-    scaleBox.style.transformOrigin = `0 ${(window.innerHeight - deviseH * scale) + 'px' } 0`
-  } else {
-    document.body.classList.add('pc')
-    var scale = (window.innerHeight / deviseH).toFixed(2)
-    scaleBox.style.width = deviseW + 'px'
-    scaleBox.style.height = deviseH + 'px'
-    scaleBox.style.transform = `scale(${scale}, ${scale}) translate(${(window.innerWidth - deviseW * scale) / 2 / scale + 'px' }, 0)`
-    scaleBox.style.transformOrigin = '0 0 0'
-  }
-}
-
-getScale()
-
-let timer = null
-window.onresize = () => {
-  // console.log(timer)
-  window.clearTimeout(timer)
-  timer = setTimeout(() => {
-    getScale()
-  }, 300)
-}
-
 function scrollIntoView () {
   window.scrollTo(0, 0)
 }
